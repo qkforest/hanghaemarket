a417975aadf13f6822872fabb81135a43a29442d
quokka <qkforest@gmail.com>
2024-07-08 10:20:00 +0900

    init

---
+++ src/main/java/com/sparta/hanghaemarket/HanghaemarketApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghaemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaemarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaemarketApplication.class, args);
+    }
+
+}
